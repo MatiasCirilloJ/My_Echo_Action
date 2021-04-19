e69f7c2e56095eadf0024bdd8068edb58de03923
MatiasCirilloJ <75842869+MatiasCirilloJ@users.noreply.github.com>
2021-04-19 10:01:54 -0300

    Update my_echo_action.py

--- actions/my_echo_action.py
+++ actions/my_echo_action.py
@@ -1,6 +1,6 @@
 import sys
 import time
-import md5
+import hashlib
 import filecmp
 
 from st2common.runners.base_action import Action
@@ -8,12 +8,12 @@ from st2common.runners.base_action import Action
 class MyEchoAction(Action):
     def run(self, message):
         try:
-            hash1 = md5.new()
+            hash1 = hashlib.md5()
             hash1.update("/opt/stackstorm/packs/my_echo_action/actions/logs.txt")
             hash1.digest() # this generates the checksum
             with open("/opt/stackstorm/packs/my_echo_action/actions/logs.txt", "a") as f:
                 f.write(message + "\n")
-            hash2 = md5.new()
+            hash2 = hashlib.md5()
             hash2.update("/opt/stackstorm/packs/my_echo_action/actions/logs.txt")
             hash2.digest() # this generates the checksum
             #time.sleep(10)
