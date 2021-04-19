b43a9e83dabd24e8e4981032d7e91ab6f3532a42
MatiasCirilloJ <75842869+MatiasCirilloJ@users.noreply.github.com>
2021-04-19 10:32:45 -0300

    Update my_echo_action.py

--- actions/my_echo_action.py
+++ actions/my_echo_action.py
@@ -14,10 +14,10 @@ from st2common.runners.base_action import Action
 class MyEchoAction(Action):
     def run(self, message):
         
-        with open("/opt/stackstorm/packs/my_echo_action/actions/logs.txt", "rw") as f:
-            f.write(hashlib.md5(f.read()).hexdigest())
+        with open("/opt/stackstorm/packs/my_echo_action/actions/logs.txt", "rb") as f:
+            print(hashlib.md5(f.read()).hexdigest())
         with open("/opt/stackstorm/packs/my_echo_action/actions/logs.txt", "a") as f:
             f.write(message + "\n")
-        with open("/opt/stackstorm/packs/my_echo_action/actions/logs.txt", "rw") as f:
-            f.write(hashlib.md5(f.read()).hexdigest())
+        with open("/opt/stackstorm/packs/my_echo_action/actions/logs.txt", "rb") as f:
+            print(hashlib.md5(f.read()).hexdigest())
         return (True, message)
