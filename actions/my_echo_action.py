b461045a1fbf9cd4646c814010a3f6bba6c35817
MatiasCirilloJ <75842869+MatiasCirilloJ@users.noreply.github.com>
2021-04-19 09:57:17 -0300

    Update my_echo_action.py

--- actions/my_echo_action.py
+++ actions/my_echo_action.py
@@ -1,16 +1,28 @@
 import sys
 import time
+import md5
+import filecmp
 
 from st2common.runners.base_action import Action
 
 class MyEchoAction(Action):
     def run(self, message):
         try:
+            hash1 = md5.new()
+            hash1.update("/opt/stackstorm/packs/my_echo_action/actions/logs.txt")
+            hash1.digest() # this generates the checksum
             with open("/opt/stackstorm/packs/my_echo_action/actions/logs.txt", "a") as f:
                 f.write(message + "\n")
+            hash2 = md5.new()
+            hash2.update("/opt/stackstorm/packs/my_echo_action/actions/logs.txt")
+            hash2.digest() # this generates the checksum
             time.sleep(10)
-            with open("/opt/stackstorm/packs/my_echo_action/actions/logs.txt", "a") as f:
-                f.write("Listoo" + "\n")
+            if filecmp.cmp(hash1, hash2):
+                with open("/opt/stackstorm/packs/my_echo_action/actions/logs.txt", "a") as f:
+                    f.write("True" + "\n")
+            else:
+                with open("/opt/stackstorm/packs/my_echo_action/actions/logs.txt", "a") as f:
+                    f.write("False" + "\n")
             return (True, message)
         except IOError:
             return (False, message)
