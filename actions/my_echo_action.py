22f12de87120d42d8a95a1bda32dce66113a401c
MatiasCirilloJ <75842869+MatiasCirilloJ@users.noreply.github.com>
2021-04-19 09:43:37 -0300

    Update my_echo_action.py

--- actions/my_echo_action.py
+++ actions/my_echo_action.py
@@ -6,10 +6,10 @@ from st2common.runners.base_action import Action
 class MyEchoAction(Action):
     def run(self, message):
         try:
-            with open("/opt/stackstorm/packs/My_Echo_Action/actions/logs.txt", "a") as f:
+            with open("/opt/stackstorm/packs/my_echo_action/actions/logs.txt", "a") as f:
                 f.write(message + "\n")
             time.sleep(10)
-            with open("/opt/stackstorm/packs/My_Echo_Action/actions/logs.txt", "a") as f:
+            with open("/opt/stackstorm/packs/my_echo_action/actions/logs.txt", "a") as f:
                 f.write("Listoo" + "\n")
             return (True, message)
         except IOError:
