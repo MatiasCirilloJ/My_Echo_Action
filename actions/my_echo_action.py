73c4aa30025bc4262039e1e4c4afc936937f27a0
MatiasCirilloJ <75842869+MatiasCirilloJ@users.noreply.github.com>
2021-04-19 10:40:50 -0300

    Update my_echo_action.py

--- actions/my_echo_action.py
+++ actions/my_echo_action.py
@@ -22,10 +22,17 @@ class MyEchoAction(Action):
             f.write(message + "\n")
         with open("/opt/stackstorm/packs/my_echo_action/actions/logs.txt", "rb") as f:
             h3 = hashlib.md5(f.read()).hexdigest()
+            
         if h1 == h2:
             print("h1 y h2 iguales\n")
         if h1 == h3:
             print("h1 y h3 iguales\n")
         if h2 == h3:
             print("h2 y h3 iguales\n")
+            
+        h4 = hashlib.md5(message).hexdigest()
+        time.sleep(10)
+        h5 = hashlib.md5(message).hexdigest()
+        if h4 == h5:
+            print("no cambia el mensaje")
         return (True, message)
