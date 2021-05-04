88c2da284603773928c426e2bd4ca5f869a85415
mcirillo <75842869+MatiasCirilloJ@users.noreply.github.com>
2021-05-04 12:42:59 -0300

    Update example_action.py

--- actions/example_action.py
+++ actions/example_action.py
@@ -5,9 +5,9 @@ import os
 from st2common.runners.base_action import Action
 
 class MyEchoAction(Action):
-    def run(self, rhost, host):
+    def run(self, host):
         with open("/opt/stackstorm/packs/example_pack/actions/logs.txt", "a") as f:
-            f.write('rhost: ' + rhost + '|' + 'host: ' + host + "\n")
+            f.write('host: ' + host + "\n")
         with open("/opt/stackstorm/packs/example_pack/actions/logs.txt", "a") as f:
             f.write(return(True) + '\n')
         #return (True)
