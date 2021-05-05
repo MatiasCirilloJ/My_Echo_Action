c7d979c10ab693908db4ee697f1c6b48a2dbd4ce
mcirillo <75842869+MatiasCirilloJ@users.noreply.github.com>
2021-05-05 13:41:11 -0300

    Update example_action_1.py

--- actions/example_action_1.py
+++ actions/example_action_1.py
@@ -14,4 +14,4 @@ class MyEchoAction(Action):
         execu = subprocess.check_output("st2 execution get {}".format(id_execution), shell=True)
         with open("/opt/stackstorm/packs/example_pack/actions/logs.txt", "a") as f:
             f.write("-----------\n" + execu.decode("utf-8") + "\n" + "-----------\n")
-        return (True, id_execution)
+        return (True, str(id_execution))
