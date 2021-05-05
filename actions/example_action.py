006b5508e23fc5ae89f1f9c8864424876d0fe6ea
mcirillo <75842869+MatiasCirilloJ@users.noreply.github.com>
2021-05-05 11:49:02 -0300

    Update example_action.py

--- actions/example_action.py
+++ actions/example_action.py
@@ -8,7 +8,7 @@ class MyEchoAction(Action):
     def run(self, host):
         with open("/opt/stackstorm/packs/example_pack/actions/logs.txt", "a") as f:
             f.write('host: ' + host + "\n")
-        execution = local.format("st2 execution list -n 1 -j")
+        execution = os.system("st2 run core.local cmd='st2 execution list -n 1 -j'")
         with open("/opt/stackstorm/packs/service_remediations_pack/actions/logs.txt", "a") as f:
             f.write("-----------\n" + execution + "\n" + "-----------\n")
         return (True)
