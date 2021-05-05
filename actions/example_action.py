eb199dad5c8511e9a3583912cf26d4766d466bad
mcirillo <75842869+MatiasCirilloJ@users.noreply.github.com>
2021-05-05 12:25:21 -0300

    Update example_action.py

--- actions/example_action.py
+++ actions/example_action.py
@@ -1,6 +1,7 @@
 import sys
 import time
-import os
+import json
+import subprocess
 
 from st2common.runners.base_action import Action
 
@@ -8,7 +9,9 @@ class MyEchoAction(Action):
     def run(self, host):
         with open("/opt/stackstorm/packs/example_pack/actions/logs.txt", "a") as f:
             f.write('host: ' + host + "\n")
-        execution = os.system("st2 execution list -n 1 -j")
+        execution = subprocess.check_output("st2 execution list -n 1 -j", shell=True)
+        id_ = json.loads(execution)[0]["id"]
+        execu = subprocess.check_output("st2 execution get {}".format(id_), shell=True)
         with open("/opt/stackstorm/packs/example_pack/actions/logs.txt", "a") as f:
-            f.write("-----------\n" + str(execution) + "\n" + "-----------\n")
-        return (True)
+            f.write("-----------\n" + execu.decode("utf-8") + "\n" + "-----------\n")
+        return (True, execution)
