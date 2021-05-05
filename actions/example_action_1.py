099ee6f248a51d68bfd4f3c43bbb74f7aa03029d
mcirillo <75842869+MatiasCirilloJ@users.noreply.github.com>
2021-05-05 13:40:42 -0300

    Update example_action_1.py

--- actions/example_action_1.py
+++ actions/example_action_1.py
@@ -10,8 +10,8 @@ class MyEchoAction(Action):
         with open("/opt/stackstorm/packs/example_pack/actions/logs.txt", "a") as f:
             f.write('host: ' + host + "\n")
         execution = subprocess.check_output("st2 execution list -n 1 -j", shell=True)
-        id_ = json.loads(execution)[0]["id"]
-        execu = subprocess.check_output("st2 execution get {}".format(id_), shell=True)
+        id_execution = json.loads(execution)[0]["id"]
+        execu = subprocess.check_output("st2 execution get {}".format(id_execution), shell=True)
         with open("/opt/stackstorm/packs/example_pack/actions/logs.txt", "a") as f:
             f.write("-----------\n" + execu.decode("utf-8") + "\n" + "-----------\n")
-        return (True, execution)
+        return (True, id_execution)
